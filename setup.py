a295b501a55d906270ada0b9ffded04a272bd794
thimothe <das.thimothe@gmail.com>
2019-11-08 01:51:08 +0100

    requirement.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
-
+import sys
 #subprocess.call(["ipconfig"])
 # subprocess.run(["sudo apt update"])
 # subprocess.run(["sudo apt install python3-pip"])
@@ -14,6 +14,21 @@ from subprocess import Popen, PIPE, STDOUT
 # subprocess.run(["tar -xvzf geckodriver-v0.26.0-linux64.tar.gz"])
 # subprocess.run(["chmod +x geckodriver"])
 # subprocess.run(["sudo apt-get install firefox"])
-p = Popen(['sudo apt-get install firefox'], stdin=PIPE, shell=True)
-p.communicate(input='y')
-p.communicate(input='\n')
+packages = ["sudo apt update", 
+"sudo apt install python3-pip", 
+"pip3 install selenium", 
+"pip3 install clipboard", 
+"pip3 install pymongo",
+"pip3 install boto3",
+"pip3 install streamlink",
+"pip3 install keyboard",
+"wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz",
+"tar -xvzf geckodriver-v0.26.0-linux64.tar.gz",
+"chmod +x geckodriver",
+"sudo apt-get install firefox"
+ ]
+
+def install(package):
+    subprocess.call([sys.executable, package])
+for package in packages:
+	install(package)
